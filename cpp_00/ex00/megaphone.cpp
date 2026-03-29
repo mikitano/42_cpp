605ba04b000989a492359ba1acf64f9dbfe6a2be
mikitano <magakitano@yahoo.com.br>
2026-03-29 16:42:27 -0300

    42 header

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -1,5 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkitano <mkitano@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/28 15:01:27 by mkitano           #+#    #+#             */
+/*   Updated: 2026/03/28 15:03:57 by mkitano          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
-#include <cctypes>
+#include <cctype>
 #include <string>
 
 int	main(int ac, char *av[])
@@ -13,7 +25,7 @@ int	main(int ac, char *av[])
 	for (int i = 1; i < ac; i++)
 	{
 		std::string	str(av[i]);
-		for (size_t j = 0; j < str.lenght(); j++)
+		for (size_t j = 0; j < str.length(); j++)
 			std::cout << (char)std::toupper(str[j]);
 		if (i < ac - 1)
 			std::cout << " ";
