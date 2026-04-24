22a4c5330638ea160c102848f78bdd47fe58925a
magakitano <magakitano@yahoo.com.br>
2026-04-23 22:19:59 -0300

    ex02 main and Makefile

--- cpp_01/ex02/main.cpp
+++ cpp_01/ex02/main.cpp
@@ -10,3 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+
+int main(void)
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << "Adress of str: " << &str << std::endl;
+	std::cout << "Adress of stringPTR: " << stringPTR << std::endl;
+	std::cout << "Adress of stringREF: " << &stringREF << std::endl << std::endl;
+
+	std::cout << "Value of str: " << str << std::endl;
+	std::cout << "Value of stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value of stringREF: " << stringREF << std::endl;
+
+	return 0;
+}
