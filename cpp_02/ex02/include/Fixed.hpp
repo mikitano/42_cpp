28848d875f89356ecb6821c4ec1bfb1b2e979a47
magakitano <magakitano@yahoo.com.br>
2026-05-03 00:14:01 -0300

    work on ex02

---
+++ cpp_02/ex02/include/Fixed.hpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkitano <mkitano@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/01 23:48:11 by mkitano           #+#    #+#             */
+/*   Updated: 2026/05/01 23:48:11 by mkitano          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <cmath>
+
+class Fixed {
+	private:
+		int _rawBits;
+		static const int _fracBits = 8;
+
+	public:
+		Fixed(void);
+		Fixed(const int value);
+		Fixed(const float value);
+		Fixed(const Fixed &obj);
+		Fixed& operator=(const Fixed &obj);
+		~Fixed(void);
+
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+		float toFloat(void) const;
+		int toInt(void) const;
+
+		bool operator>(const Fixed& other) const;
+		bool operator<(const Fixed& other) const;
+		bool operator>=(const Fixed& other) const;
+		bool operator<=(const Fixed& other) const;
+		bool operator==(const Fixed& other) const;
+		bool operator!=(const Fixed& other) const;
+
+		Fixed operator+(const Fixed& other) const;
+		Fixed operator-(const Fixed& other) const;
+		Fixed operator*(const Fixed& other) const;
+		Fixed operator/(const Fixed& other) const;
+
+
+};
+
+std::ostream& operator<<(std::ostream& stream, const Fixed& obj);
---
+++ cpp_02/ex02/src/Fixed.cpp
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkitano <mkitano@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/01 14:48:47 by mkitano           #+#    #+#             */
+/*   Updated: 2026/05/01 17:34:04 by mkitano          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed(): _rawBits(0) {
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int value) {
+	std::cout << "Int constructor called" << std::endl;
+	_rawBits = value << _fracBits;
+}
+
+Fixed::Fixed(const float value) {
+	std::cout << "Float constructor called" << std::endl;
+	_rawBits = roundf(value * (1 << _fracBits));
+}
+
+Fixed::Fixed(const Fixed &obj) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = obj;
+}
+
+Fixed& Fixed::operator=(const Fixed &obj) {
+	if (this != &obj)
+		this->_rawBits = obj._rawBits;
+	std::cout << "Copy assignment operator called" << std::endl;
+	return *this;
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const {
+	return this->_rawBits;
+}
+
+void Fixed::setRawBits(int const raw) {
+	this->_rawBits = raw;
+}
+
+float Fixed::toFloat(void) const {
+	return static_cast<float>(this->getRawBits()) / (1 << _fracBits);
+}
+
+int Fixed::toInt(void) const {
+	return (this->getRawBits() >> _fracBits);
+}
+
+bool Fixed::operator>(const Fixed& other) const {
+	return (this->getRawBits() > other.getRawBits());
+}
+
+bool Fixed::operator<(const Fixed& other) const {
+	return (other > *this);
+}
+
+bool Fixed::operator>=(const Fixed& other) const {
+	return !(*this < other);
+}
+
+bool Fixed::operator<=(const Fixed& other) const {
+	return !(*this > other);
+}
+
+bool Fixed::operator==(const Fixed& other) const {
+	return (this->_rawBits == other._rawBits);
+}
+
+bool Fixed::operator!=(const Fixed& other) const {
+	return !(*this == other);
+}
+
+Fixed Fixed::operator+(const Fixed& other) const {
+	Fixed result;
+	result.setRawBits(this->_rawBits + other._rawBits);
+	result (result);
+}
+
+Fixed Fixed::operator-(const Fixed& other) const {
+	Fixed result;
+	result.setRawBits(this->_rawBits - other._rawBits);
+	result (result);
+}
+
+Fixed Fixed::operator*(const Fixed& other) const {
+	Fixed result;
+	result.setRawBits((this->_rawBits * other._rawBits) >> _fracBits);
+	result (result);
+}
+
+Fixed Fixed::operator/(const Fixed& other) const {
+	Fixed result;
+	result.setRawBits((this->_rawBits << _fracBits) / other._rawBits);
+	result (result);
+}
+
+std::ostream& operator<<(std::ostream& stream, const Fixed& obj) {
+	stream << obj.toFloat();
+	return stream;
+}
