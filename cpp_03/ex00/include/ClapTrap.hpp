f9d8bcff96252d4c6e23dfef0c1dda544e314d80
mikitano <magakitano@yahoo.com.br>
2026-05-03 17:09:39 -0300

    start cpp03

---
+++ cpp_03/ex00/include/ClapTrap.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkitano <mkitano@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/03 16:26:39 by mkitano           #+#    #+#             */
+/*   Updated: 2026/05/03 16:35:08 by mkitano          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class ClapTrap {
+	private:
+		std::string _name;
+		int _hitPoint;
+		int _enegyPoint;
+		int _attackDam;
+		
+	public:
+		ClapTrap();
+		ClapTrap(std::string& name);
+		ClapTrap(const ClapTrap& rhs);
+		ClapTrap& operator=(const ClapTrap& rhs);
+		~ClapTrap();
+
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+};
